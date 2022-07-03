45d9daa3f34765ac490a0ff647d33706ede16f78
bans04 <bansodednyanadeo@gmail.com>
2022-07-03 23:49:30 +0530

    Ability to create Linked List by adding 30 and 56 to 70.

--- src/com/linkedlist/LinkedList.java
+++ src/com/linkedlist/LinkedList.java
@@ -1,18 +1,43 @@
 package com.linkedlist;
 /*
  * Lets create a simple Linked List of 56, 30 and 70
+ * Ability to create Linked List by adding 30 and 56 to 70
  * 
  * @ Dnyandeo Banosde
  * 
  * */
 
 public class LinkedList {
+	Node head;
+
+	public void addNodeMethod(int data) {
+		Node newNode = new Node(data);
+		if (head == null) {
+			head = newNode;
+		} else {
+			Node temp = head;
+			head = newNode;
+			newNode.next = temp;
+		}
+	}
+	
+	public void display() {
+		Node temp = head;
+		if (head == null) {
+			System.out.println("Linked list is empty");
+		} else {
+			while (temp != null) {
+				System.out.println(temp.data + "->");
+				temp = temp.next;
+			}
+		}
+	}
 
 	public static void main(String[] args) {
-        Node firstNode = new Node(56);
-        Node secondNode = new Node(30);
-        Node thirdNode = new Node(70);
-        firstNode.next = secondNode;
-        secondNode.next = thirdNode;
+		LinkedList obj = new LinkedList();
+		obj.addNodeMethod(70);
+		obj.addNodeMethod(30);
+		obj.addNodeMethod(56);
+		obj.display();
 	}
 }
